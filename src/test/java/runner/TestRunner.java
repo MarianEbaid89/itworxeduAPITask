d71707ad5cc2d596fdee687b46a0b8a8201b672e
m.ebaid <m.ebaid@minapharm.com>
2023-11-21 14:38:37 +0200

    21-11

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,2 +1,15 @@
-package runner;public class TestRunner {
+package runner;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features",
+        glue = "steps",
+        plugin = {"pretty","html:target/cucumber.html",
+                "json:target/cucumber.json",
+                "junit:target/cukes.xml",
+                "rerun:target/rerun.txt"}
+)
+public class TestRunner {
 }
